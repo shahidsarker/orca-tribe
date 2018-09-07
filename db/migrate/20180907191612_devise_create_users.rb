dd7d5485e23cae6455a93bafbb47ab104aeb066a
imani alexis <alexis.bailey37@gmail.com>
2018-09-07 15:46:28 -0400

    added first_name,last_name,location,avatar and bio to devise User table under db > migrate

--- db/migrate/20180907191612_devise_create_users.rb
+++ db/migrate/20180907191612_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false, default: ""
+      t.string :last_name,          null: false, default: ""
+      t.string :location,           null: false, default: ""
+      t.string :avatar,             null: false, default: ""
+      t.text   :bio,                null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
