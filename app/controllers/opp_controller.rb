2305bb8aae7fae6323c85f39aeb958c47230892b
Milly R <millz118@gmail.com>
2018-09-07 15:32:58 -0400

    controller for org/opp

--- app/controllers/opp_controller.rb
+++ app/controllers/opp_controller.rb
@@ -1,2 +1,22 @@
 class OppController < ApplicationController
+    def list
+    end
+    
+    def show
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def delete
+    end
 end
--- app/controllers/org_controller.rb
+++ app/controllers/org_controller.rb
@@ -1,2 +1,22 @@
 class OrgController < ApplicationController
+    def list
+    end
+    
+    def show
+    end
+    
+    def new
+    end
+    
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def delete
+    end
 end
