ffd38679bc4ef046ed32132bfdeb41b04cba7b6c
imani alexis <alexis.bailey37@gmail.com>
2018-09-11 12:07:59 -0400

    updated models opp and user to reflect the manytomany relationship with user_opss

--- app/controllers/welcome_page_controller.rb
+++ app/controllers/welcome_page_controller.rb
@@ -3,4 +3,20 @@ class WelcomePageController < ApplicationController
     def index
 
     end
+
+    def show
+        if current_user
+            puts '******************'
+            puts current_user
+            puts '******************'
+            @current_person = current_user
+           
+            @first_name = @current_person.first_name
+          else
+            redirect_to new_user_session_path, notice: 'Please login or sign-up.'
+          end
+    end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :user_opps
+  has_many :opps, through: :user_opps #volunteers
 
   # acts as voter gem -Shahid
   acts_as_voter
