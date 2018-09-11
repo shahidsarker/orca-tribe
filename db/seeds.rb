bf71429d7ca662add022d0e5dd8695b97c4c0b13
Shahid <shahid.sarker@gmail.com>
2018-09-11 02:12:20 -0400

    WET API seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -125,3 +125,94 @@
     #     api_opp_id: 34,
     #     org_id: 1
     # )
+
+require 'httparty'
+
+NYCSERV_KEY = ENV['NYCSERV_KEY']
+NYCSERV_ID = ENV['NYCSERV_ID']
+
+# sets a random page number to access opportunities
+page_num = rand(43)
+
+# response from http GET request to organization listings
+# org_response = HTTParty.get("https://api.nycservice.org/organizations/index.php?rest_key=#{NYCSERV_KEY}&account_id=#{NYCSERV_ID}&list_page=#{page_num}&output=json&max_records=10")
+
+# creates a list of organization_id values from the org listing http response
+# org_list = []
+# org_response['organizations'].each do |org|
+#   org_list.push(org['organization_id'])
+# end
+
+# org_id = org_list.sample
+# org_detail_response =  HTTParty.get("https://api.nycservice.org/organizations/index.php?account_id=#{NYCSERV_ID}&rest_key=#{NYCSERV_KEY}&output=json&org_id=#{org_id}")
+
+# pp org_detail_response
+
+# get request random opportunities
+random_opps_response = HTTParty.get("https://api.nycservice.org/opportunities/index.php?rest_key=#{NYCSERV_KEY}&account_id=#{NYCSERV_ID}&output=json&list_page=#{page_num}&max_records=5")
+
+# pp random_opps_response
+
+# create a list of random opportunity IDs
+random_opp_list = []
+random_opps_response['opportunities'].each do |opp|
+  random_opp_list.push(opp['opportunity_id'])
+end
+
+# create a hash of organizations using the random opportunity list
+orgs_list = {}
+random_opp_list.each do |opp_id|
+  opp_detail_response = HTTParty.get("https://api.nycservice.org/opportunities/index.php?rest_key=#{NYCSERV_KEY}&account_id=#{NYCSERV_ID}&output=json&opp_id=#{opp_id}")
+  # set a key-value pair of org_id => []
+  orgs_list[opp_detail_response['opportunity']['organization_id']] = []
+end
+
+# for each key in the organization list, push in its opp_id
+orgs_list.each_pair do |org_id,opp_array|
+  org_detail_response = HTTParty.get("https://api.nycservice.org/organizations/index.php?account_id=#{NYCSERV_ID}&rest_key=#{NYCSERV_KEY}&output=json&org_id=#{org_id}")
+  # loop through the opportunities for the organization and save the opp_id in the array
+  org_detail_response['organization']['opportunities'].each do |opp|
+    opp_array.push(opp['opportunity_id'])
+  end
+
+  org_name = org_detail_response['organization']['title']
+  org_summary = org_detail_response['organization']['summary']
+  org_add_hash = org_detail_response['organization']['organization_contact']
+  org_address = "#{org_add_hash['street1']} #{org_add_hash['street2']}, #{org_add_hash['locality']}, #{org_add_hash['region']}, #{org_add_hash['postalcode']}"
+  org_url = org_detail_response['organization']['organization_url']
+  org_thumb = org_detail_response['organization']['thumbnail_url']
+  org_phone = 'n/a'
+  api_organization_id = org_detail_response['organization']['organization_id'].to_i
+  this_org = Org.create(name: org_name,
+                        summary: org_summary,
+                        address: org_address,
+                        website: org_url,
+                        thumbnail: org_thumb,
+                        phone: org_phone,
+                        api_org_id: api_organization_id)
+
+  opp_array.each do |opp_id|
+    opp_detail_response = HTTParty.get("https://api.nycservice.org/opportunities/index.php?rest_key=#{NYCSERV_KEY}&account_id=#{NYCSERV_ID}&output=json&opp_id=#{opp_id}")
+    opp_title = opp_detail_response['opportunity']['title']
+    opp_start = Time.at(opp_detail_response['opportunity']['start_date'].to_i)
+    opp_end = Time.at(opp_detail_response['opportunity']['end_date'].to_i)
+    opp_summary = opp_detail_response['opportunity']['summary']
+    opp_vols = opp_detail_response['opportunity']['vol_requests'].to_i
+    opp_recurrence = opp_detail_response['opportunity']['recurrence_type'] == 'ongoing'
+    opp_reqs = opp_detail_response['opportunity']['requirements']
+    opp_add_hash = opp_detail_response['opportunity']['lccontact']
+    opp_address = "#{opp_add_hash['street1']} #{opp_add_hash['street2']}, #{opp_add_hash['locality']}, #{opp_add_hash['region']}, #{opp_add_hash['postalcode']}"
+    api_opportunity_id = opp_detail_response['opportunity']['organization_id'].to_i
+
+    Opp.create(title: opp_title,
+               start_date:  opp_start,
+               end_date:  opp_end,
+               summary: opp_summary,
+               vol_request: opp_vols,
+               recurrence: opp_recurrence,
+               requirement: opp_reqs,
+               location: opp_address,
+               api_opp_id: api_opportunity_id,
+               org: this_org)
+  end
+end
