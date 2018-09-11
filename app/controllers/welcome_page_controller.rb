3dd650507948846a4f7edfc6a5a8095ada4d79c2
imani alexis <alexis.bailey37@gmail.com>
2018-09-11 13:34:18 -0400

    added route for profile page that shows all the info associated to the current user

--- app/controllers/welcome_page_controller.rb
+++ app/controllers/welcome_page_controller.rb
@@ -4,14 +4,16 @@ class WelcomePageController < ApplicationController
 
     end
 
-    def show
+    def specific_user
         if current_user
-            puts '******************'
-            puts current_user
-            puts '******************'
+            
             @current_person = current_user
            
             @first_name = @current_person.first_name
+            @last_name = @current_person.last_name
+            @location = @current_person.location
+            @bio = @current_person.bio
+            @avatar = @current_person.avatar
           else
             redirect_to new_user_session_path, notice: 'Please login or sign-up.'
           end
