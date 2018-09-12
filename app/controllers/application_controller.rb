5f950f32d34fc5934dd01ce66c67a334fe05b0d4
Milly R <millz118@gmail.com>
2018-09-12 18:10:44 -0400

    more styling on pages..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_orgs, 
     
   def set_orgs
-      @orgs = Org.all.limit(3)
+      @org = Org.order("created_at desc").limit(3)
     end
     
     def after_sign_in_path_for(current_user)
