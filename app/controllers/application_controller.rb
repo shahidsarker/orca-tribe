e8b955a0cbaf9e4d6f5f738a741cea555fef5c96
Milly R <millz118@gmail.com>
2018-09-13 11:16:36 -0400

    styling opps profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_orgs, 
     
   def set_orgs
-      @org = Org.order("created_at desc").limit(3)
+      @orgs = Org.all.limit(3)
     end
     
     def after_sign_in_path_for(current_user)
