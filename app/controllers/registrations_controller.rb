628575b611913a7c6c66a53c35586773bf812875
imani alexis <alexis.bailey37@gmail.com>
2018-09-13 15:09:48 -0400

    updated the redirect to kick you back to users profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
+    
+    protected
+
+    def after_update_path_for(resource)
+        puts 'this is happening yoyo mama'
+        flash[:notice] = "Account succesfully updated"
+        profile_path
+    end
+    
+    
+    
+    
     private
 
   def sign_up_params
